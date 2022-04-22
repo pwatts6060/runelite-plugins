d4b4f5d8a5c4314a3df25a97672a1e4bff1a694e
Patrick Watts <patrickwatts@g.harvard.edu>
2022-04-22 16:09:06 +0400

    Add Location code from star-calling-assist plugin

--- src/main/java/com/starinfo/Location.java
+++ src/main/java/com/starinfo/Location.java
@@ -1,109 +1,139 @@
 package com.starinfo;
 
+import java.util.HashMap;
+import java.util.Map;
+import net.runelite.api.Point;
 import net.runelite.api.coords.WorldPoint;
 
 public class Location
 {
-	public static final Location UNKNOWN_LOCATION = new Location(0, 0, 0, "Unknown location");
-
-	private static final Location[] values = new Location[]{
-		new Location(3443, 3018, 0, "Dwarven Mine"),
-		new Location(3348, 3030, 0, "Mining Guild"),
-		new Location(3355, 2906, 0, "West Falador mine"),
-		new Location(3474, 2882, 0, "Taverley"),
-		new Location(3280, 2940, 0, "Crafting Guild"),
-		new Location(3241, 2974, 0, "Rimmington mine"),
-		new Location(3238, 2822, 0, "South Crandor mine"),
-		new Location(3296, 2835, 0, "North Crandor mine"),
-		new Location(3221, 2736, 0, "North Brimhaven mine"),
-		new Location(3143, 2742, 0, "South Brimhaven mine"),
-		new Location(3037, 2845, 0, "Karamja Jungle mine"),
-		new Location(2999, 2827, 0, "Shilo Village mine"),
-		new Location(2964, 2571, 0, "Feldip Hunter area"),
-		new Location(2993, 2630, 0, "Rantz's cave"),
-		new Location(2858, 2567, 0, "Corsair Cove"),
-		new Location(2886, 2483, 0, "Corsair Cove Resource Area"),
-		new Location(2842, 2468, 0, "Myths' Guild"),
-		new Location(2792, 2200, 0, "Isle of Souls mine"),
-		new Location(3814, 3774, 0, "Fossil Island mine"),
-		new Location(3801, 3818, 0, "Volcanic Mine"),
-		new Location(2969, 3686, 0, "Mos Le'Harmless"),
-		new Location(3699, 2683, 0, "Rellekka mine"),
-		new Location(3683, 2727, 0, "Keldagrim entrance mine"),
-		new Location(3887, 2528, 0, "Miscellania mine"),
-		new Location(3814, 2393, 0, "Jatizso mine"),
-		new Location(3832, 2375, 0, "Central Fremennik Isles mine"),
-		new Location(3938, 2139, 0, "Lunar Isle mine"),
-		new Location(3493, 1778, 0, "Hosidius mine"),
-		new Location(3648, 1597, 0, "Shayzien mine"),
-		new Location(3709, 1769, 0, "Piscarilius mine"),
-		new Location(3853, 1760, 0, "Dense essence mine"),
-		new Location(3840, 1437, 0, "Lovakite mine"),
-		new Location(3747, 1534, 0, "Lovakengj"),
-		new Location(3434, 2804, 0, "Catherby"),
-		new Location(3086, 2602, 0, "Yanille"),
-		new Location(3141, 2624, 0, "Port Khazard mine"),
-		new Location(3333, 2705, 0, "Legends' Guild mine"),
-		new Location(3478, 2589, 0, "Coal Trucks"),
-		new Location(3233, 2608, 0, "South-east Ardougne mine"),
-		new Location(3651, 1210, 0, "Kebos Lowlands mine"),
-		new Location(3817, 1279, 0, "Mount Karuulm mine"),
-		new Location(3816, 1322, 0, "Mount Karuulm"),
-		new Location(3564, 1258, 0, "Mount Quidamortem"),
-		new Location(3298, 3296, 0, "Al Kharid mine"),
-		new Location(3164, 3276, 0, "Al Kharid"),
-		new Location(3160, 3424, 0, "Uzer mine"),
-		new Location(2910, 3171, 0, "Desert quarry"),
-		new Location(2867, 3316, 0, "Agility Pyramid mine"),
-		new Location(2889, 3434, 0, "Nardah"),
-		new Location(3267, 3341, 0, "Duel Arena"),
-		new Location(3155, 3230, 0, "East Lumbridge Swamp mine"),
-		new Location(3150, 3153, 0, "West Lumbridge Swamp mine"),
-		new Location(3235, 3094, 0, "Draynor Village"),
-		new Location(3408, 3258, 0, "Varrock"),
-		new Location(3353, 3290, 0, "South-east Varrock mine"),
-		new Location(3362, 3175, 0, "South-west Varrock mine"),
-		new Location(3485, 3505, 0, "Canifis"),
-		new Location(3219, 3500, 0, "Burgh de Rott"),
-		new Location(3233, 3451, 0, "Abandoned Mine"),
-		new Location(3214, 3650, 0, "Ver Sinhaza"),
-		new Location(3340, 3635, 0, "Daeyalt essence mine"),
-		new Location(3635, 2341, 0, "Piscatoris mine"),
-		new Location(3490, 2444, 0, "Grand Tree"),
-		new Location(3436, 2448, 0, "Tree Gnome Stronghold"),
-		new Location(3158, 2269, 0, "Isafdar mine"),
-		new Location(3269, 2318, 0, "Arandar mine"),
-		new Location(3163, 2329, 0, "Lletya"),
-		new Location(6055, 3274, 0, "Trahaearn mine"),
-		new Location(3409, 2173, 0, "Mynydd mine"),
-		new Location(3569, 3108, 0, "South Wilderness mine"),
-		new Location(3593, 3018, 0, "South-west Wilderness mine"),
-		new Location(3756, 3093, 0, "Bandit Camp mine"),
-		new Location(3887, 3057, 0, "Lava Maze runite mine"),
-		new Location(3932, 3188, 0, "Resource Area"),
-		new Location(3962, 3091, 0, "Mage Arena"),
-		new Location(3940, 3049, 0, "Pirates' Hideout mine"),
-		UNKNOWN_LOCATION,
+	private static final Map<Point, String> LOCATION_NAMES = new HashMap<Point, String>()
+	{
+		{
+	    /*
+	    ASGARNIA
+	     */
+			put(new Point(2974, 3241), "Rimmington mine");
+			put(new Point(2940, 3280), "Crafting guild");
+			put(new Point(2906, 3355), "West Falador mine");
+			put(new Point(3030, 3348), "East Falador bank");
+			put(new Point(3018, 3443), "North Dwarven Mine entrance");
+			put(new Point(2882, 3474), "Taverley house portal");
+	    /*
+	    CRANDOR/KARAMJA
+	     */
+			put(new Point(2736, 3221), "Brimhaven northwest gold mine");
+			put(new Point(2742, 3143), "Brimhaven south dungeon entrance");
+			put(new Point(2845, 3037), "Nature Altar mine north of Shilo");
+			put(new Point(2827, 2999), "Shilo Village gem mine");
+			put(new Point(2835, 3296), "North Crandor");
+			put(new Point(2822, 3238), "South Crandor");
+	    /*
+	    DESERT
+	     */
+			put(new Point(3296, 3298), "Al Kharid mine");
+			put(new Point(3276, 3164), "Al Kharid bank");
+			put(new Point(3341, 3267), "Duel Arena");
+			put(new Point(3424, 3160), "Nw of Uzer (Eagle's Eyrie)");
+			put(new Point(3434, 2889), "Nardah bank");
+			put(new Point(3316, 2867), "Agility Pyramid mine");
+			put(new Point(3171, 2910), "Desert Quarry mine");
+	    /*
+	    FELDIP HILLS/ISLE OF SOULS
+	     */
+			put(new Point(2567, 2858), "Corsair Cove bank");
+			put(new Point(2483, 2886), "Corsair Resource Area");
+			put(new Point(2468, 2842), "Myths' Guild");
+			put(new Point(2571, 2964), "Feldip Hills (aks fairy ring)");
+			put(new Point(2630, 2993), "Rantz cave");
+			put(new Point(2200, 2792), "Soul Wars south mine");
+	    /*
+	    FOSSIL ISLAND/MOS LE HARMLESS
+	     */
+			put(new Point(3818, 3801), "Fossil Island Volcanic Mine entrance");
+			put(new Point(3774, 3814), "Fossil Island rune rocks");
+			put(new Point(3686, 2969), "Mos Le'Harmless west bank");
+	    /*
+	    FREMENNIK/LUNAR ISLE
+	     */
+			put(new Point(2727, 3683), "Keldagrim entrance mine");
+			put(new Point(2683, 3699), "Rellekka mine");
+			put(new Point(2393, 3814), "Jatizso mine entrance");
+			put(new Point(2375, 3832), "Neitiznot south of rune rock");
+			put(new Point(2528, 3887), "Miscellania mine (cip fairy ring)");
+			put(new Point(2139, 3938), "Lunar Isle mine entrance");
+	    /*
+	    KANDARIN
+	     */
+			put(new Point(2602, 3086), "Yanille bank");
+			put(new Point(2624, 3141), "Port Khazard mine");
+			put(new Point(2608, 3233), "Ardougne Monastary");
+			put(new Point(2705, 3333), "South of Legends' Guild");
+			put(new Point(2804, 3434), "Catherby bank");
+			put(new Point(2589, 3478), "Coal Trucks west of Seers'");
+	    /*
+	    KOUREND
+	     */
+			put(new Point(1778, 3493), "Hosidius mine");
+			put(new Point(1769, 3709), "Port Piscarilius mine in Kourend");
+			put(new Point(1597, 3648), "Shayzien mine south of Kourend Castle");
+			put(new Point(1534, 3747), "South Lovakengj bank");
+			put(new Point(1437, 3840), "Lovakite mine");
+			put(new Point(1760, 3853), "Arceuus dense essence mine");
+	    /*
+	    KEBOS LOWLANDS
+	     */
+			put(new Point(1322, 3816), "Mount Karuulm bank");
+			put(new Point(1279, 3817), "Mount Karuulm mine");
+			put(new Point(1210, 3651), "Kebos Swamp mine");
+			put(new Point(1258, 3564), "Chambers of Xeric bank");
+	    /*
+	    MISTHALIN
+	     */
+			put(new Point(3258, 3408), "Varrock east bank");
+			put(new Point(3290, 3353), "South-east Varrock mine");
+			put(new Point(3175, 3362), "Champions' Guild mine");
+			put(new Point(3094, 3235), "Draynor Village");
+			put(new Point(3153, 3150), "West Lumbridge Swamp mine");
+			put(new Point(3230, 3155), "East Lumbridge Swamp mine");
+	    /*
+	    MORYTANIA
+	     */
+			put(new Point(3635, 3340), "Darkmeyer ess. mine entrance");
+			put(new Point(3650, 3214), "Theatre of Blood bank");
+			put(new Point(3505, 3485), "Canifis bank");
+			put(new Point(3500, 3219), "Burgh de Rott bank");
+			put(new Point(3451, 3233), "Abandoned Mine west of Burgh");
+	    /*
+	    PISCATORIS/GNOME STRONGHOLD
+	     */
+			put(new Point(2444, 3490), "West of Grand Tree");
+			put(new Point(2448, 3436), "Gnome Stronghold spirit tree");
+			put(new Point(2341, 3635), "Piscatoris (akq fairy ring)");
+	    /*
+	    TIRANNWN
+	     */
+			put(new Point(2329, 3163), "Lletya");
+			put(new Point(2269, 3158), "Isafdar runite rocks");
+			put(new Point(3274, 6055), "Priffdinas Zalcano entrance");
+			put(new Point(2318, 3269), "Arandar mine north of Lleyta");
+			put(new Point(2173, 3409), "Mynydd nw of Priffdinas");
+	    /*
+	    WILDERNESS
+	     */
+			put(new Point(3108, 3569), "Mage of Zamorak mine (lvl 7 Wildy)");
+			put(new Point(3018, 3593), "Skeleton mine (lvl 10 Wildy)");
+			put(new Point(3093, 3756), "Hobgoblin mine (lvl 30 Wildy)");
+			put(new Point(3057, 3887), "Lava maze runite mine (lvl 46 Wildy)");
+			put(new Point(3049, 3940), "Pirates' Hideout (lvl 53 Wildy)");
+			put(new Point(3091, 3962), "Mage Arena bank (lvl 56 Wildy)");
+			put(new Point(3188, 3932), "Wilderness Resource Area");
+		}
 	};
 
-	public final WorldPoint location;
-	public final String description;
-
-	Location(int x, int y, int plane, String description)
+	public static String forLocation(WorldPoint location)
 	{
-		this.location = new WorldPoint(x, y, plane);
-		this.description = description;
-	}
-
-	public static Location forLocation(WorldPoint location)
-	{
-		for (Location loc : values)
-		{
-			if (loc.location.equals(location))
-			{
-				return loc;
-			}
-		}
-		return UNKNOWN_LOCATION;
+		Point point = new Point(location.getX(), location.getY());
+		return LOCATION_NAMES.getOrDefault(point, "Unknown location: " + point.getX() + ", " + point.getY());
 	}
 }
