9a0084eac8f416da4244461d179082b2694dd887
Patrick <patrick.watts@hotmail.co.uk>
2022-07-23 03:21:31 +0400

    Replace map with array

--- src/main/java/com/starinfo/TierData.java
+++ src/main/java/com/starinfo/TierData.java
@@ -1,23 +1,17 @@
 package com.starinfo;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class TierData
 {
-	private static final Map<Integer, TierData> map = new HashMap<Integer, TierData>()
-	{
-		{
-			put(1, new TierData(1200, 1200));
-			put(2, new TierData(700, 1900));
-			put(3, new TierData(430, 2330));
-			put(4, new TierData(250, 2580));
-			put(5, new TierData(175, 2755));
-			put(6, new TierData(80, 2835));
-			put(7, new TierData(40, 2875));
-			put(8, new TierData(40, 2915));
-			put(9, new TierData(15, 2930));
-		}
+	private static final TierData[] data = new TierData[] {
+		new TierData(1200, 1200),
+		new TierData(700, 1900),
+		new TierData(430, 2330),
+		new TierData(250, 2580),
+		new TierData(175, 2755),
+		new TierData(80, 2835),
+		new TierData(40, 2875),
+		new TierData(40, 2915),
+		new TierData(15, 2930)
 	};
 
 	public final int layerDust;
@@ -31,6 +25,9 @@ public class TierData
 
 	public static TierData get(int tier)
 	{
-		return map.get(tier);
+		if (tier < 1 || tier > data.length) {
+			return null;
+		}
+		return data[tier - 1];
 	}
 }
