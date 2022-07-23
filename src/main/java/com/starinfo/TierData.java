12ea928660ce75201f69cebd46aae5cc20022bdc
Patrick <patrick.watts@hotmail.co.uk>
2022-07-23 16:13:58 +0400

    Add success chances to tier data

--- src/main/java/com/starinfo/TierData.java
+++ src/main/java/com/starinfo/TierData.java
@@ -3,24 +3,28 @@ package com.starinfo;
 public class TierData
 {
 	private static final TierData[] data = new TierData[] {
-		new TierData(1200, 1200),
-		new TierData(700, 1900),
-		new TierData(430, 2330),
-		new TierData(250, 2580),
-		new TierData(175, 2755),
-		new TierData(80, 2835),
-		new TierData(40, 2875),
-		new TierData(40, 2915),
-		new TierData(15, 2930)
+		new TierData(1200, 1200, 0.3, 1),
+		new TierData(700, 1900, 0.3, 0.57),
+		new TierData(430, 2330, 0.3, 0.49),
+		new TierData(250, 2580, 0.3, 0.44),
+		new TierData(175, 2755, 0.23, 0.3),
+		new TierData(80, 2835, 0.15, 0.2),
+		new TierData(40, 2875, 0.09, 0.13),
+		new TierData(40, 2915, 0.07, 0.1),
+		new TierData(15, 2930, 0.06, 0.07)
 	};
 
 	public final int layerDust;
 	public final int totalDust;
+	public final double lowChance;
+	public final double highChance;
 
-	TierData(int layerDust, int totalDust)
+	TierData(int layerDust, int totalDust, double lowChance, double highChance)
 	{
 		this.layerDust = layerDust;
 		this.totalDust = totalDust;
+		this.lowChance = lowChance;
+		this.highChance = highChance;
 	}
 
 	public static TierData get(int tier)
@@ -29,5 +33,9 @@ public class TierData
 			return null;
 		}
 		return data[tier - 1];
+	}
+
+	public double getChance(int level) {
+		return lowChance + (level - 1) * (highChance - lowChance) / 98;
 	}
 }
