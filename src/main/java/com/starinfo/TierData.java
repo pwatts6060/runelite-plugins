fb645390a5d10ae8cffb843b9e3ec91d33cbf639
Patrick <patrick.watts@hotmail.co.uk>
2022-07-27 23:43:37 +0400

    update TierData success chances to follow scuffed osrs linear interpolation

--- src/main/java/com/starinfo/TierData.java
+++ src/main/java/com/starinfo/TierData.java
@@ -3,15 +3,15 @@ package com.starinfo;
 public class TierData
 {
 	private static final TierData[] data = new TierData[] {
-		new TierData(1200, 1200, 0.3, 1),
-		new TierData(700, 1900, 0.3, 0.57),
-		new TierData(430, 2330, 0.3, 0.49),
-		new TierData(250, 2580, 0.3, 0.44),
-		new TierData(175, 2755, 0.23, 0.3),
-		new TierData(80, 2835, 0.15, 0.2),
-		new TierData(40, 2875, 0.09, 0.13),
-		new TierData(40, 2915, 0.07, 0.1),
-		new TierData(15, 2930, 0.06, 0.07)
+		new TierData(1200, 1200, 0.3 * 255, 1 * 255),
+		new TierData(700, 1900, 0.3 * 255, 0.57 * 255),
+		new TierData(430, 2330, 0.3 * 255, 0.49 * 255),
+		new TierData(250, 2580, 0.3 * 255, 0.44 * 255),
+		new TierData(175, 2755, 0.23 * 255, 0.3 * 255),
+		new TierData(80, 2835, 0.15 * 255, 0.2 * 255),
+		new TierData(40, 2875, 0.09 * 255, 0.13 * 255),
+		new TierData(40, 2915, 0.07 * 255, 0.1 * 255),
+		new TierData(15, 2930, 0.06 * 255, 0.07 * 255)
 	};
 
 	public final int layerDust;
@@ -36,6 +36,6 @@ public class TierData
 	}
 
 	public double getChance(int level) {
-		return lowChance + (level - 1) * (highChance - lowChance) / 98;
+		return (1 + Math.floor(lowChance * (99 - level) / 98) + Math.floor(highChance * (level - 1) / 98)) / 256;
 	}
 }
