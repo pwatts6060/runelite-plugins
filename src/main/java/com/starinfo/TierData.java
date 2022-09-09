24e5acd162d175e5e9c069cf03a28adc969dc992
Patrick <patrick.watts@hotmail.co.uk>
2022-09-09 06:00:28 +0400

    Add doubleDustChance to TierData

--- src/main/java/com/starinfo/TierData.java
+++ src/main/java/com/starinfo/TierData.java
@@ -3,15 +3,15 @@ package com.starinfo;
 public class TierData
 {
 	private static final TierData[] data = new TierData[] {
-		new TierData(1200, 1200, 0.3 * 255, 1 * 255, 12),
-		new TierData(700, 1900, 0.3 * 255, 0.57 * 255, 22),
-		new TierData(430, 2330, 0.3 * 255, 0.49 * 255, 26),
-		new TierData(250, 2580, 0.3 * 255, 0.44 * 255, 31),
-		new TierData(175, 2755, 0.23 * 255, 0.3 * 255, 48),
-		new TierData(80, 2835, 0.15 * 255, 0.2 * 255, 74),
-		new TierData(40, 2875, 0.09 * 255, 0.13 * 255, 123),
-		new TierData(40, 2915, 0.07 * 255, 0.1 * 255, 162),
-		new TierData(15, 2930, 0.06 * 255, 0.07 * 255, 244)
+		new TierData(1200, 1200, 0.3 * 255, 1 * 255, 12, 0.02),
+		new TierData(700, 1900, 0.3 * 255, 0.57 * 255, 22, 0.06),
+		new TierData(430, 2330, 0.3 * 255, 0.49 * 255, 26, 0.12),
+		new TierData(250, 2580, 0.3 * 255, 0.44 * 255, 31, 0.20),
+		new TierData(175, 2755, 0.23 * 255, 0.3 * 255, 48, 0.30),
+		new TierData(80, 2835, 0.15 * 255, 0.2 * 255, 74, 0.42),
+		new TierData(40, 2875, 0.09 * 255, 0.13 * 255, 123, 0.56),
+		new TierData(40, 2915, 0.07 * 255, 0.1 * 255, 162, 0.72),
+		new TierData(15, 2930, 0.06 * 255, 0.07 * 255, 244, 0.90)
 	};
 
 	public final int layerDust;
@@ -19,14 +19,16 @@ public class TierData
 	public final double lowChance;
 	public final double highChance;
 	public final double xp;
+	public final double doubleDustChance;
 
-	TierData(int layerDust, int totalDust, double lowChance, double highChance, double xp)
+	TierData(int layerDust, int totalDust, double lowChance, double highChance, double xp, double doubleDustChance)
 	{
 		this.layerDust = layerDust;
 		this.totalDust = totalDust;
 		this.lowChance = lowChance;
 		this.highChance = highChance;
 		this.xp = xp;
+		this.doubleDustChance = doubleDustChance;
 	}
 
 	public static TierData get(int tier)
