0a01411d7c4fe5ad4f25c75ba710b17b0b36f035
pww918 <patrickwatts@g.harvard.edu>
2023-11-02 23:37:08 +0400

    update star tier data with October 2023 changes

--- src/main/java/com/starinfo/TierData.java
+++ src/main/java/com/starinfo/TierData.java
@@ -3,15 +3,15 @@ package com.starinfo;
 public class TierData
 {
 	private static final TierData[] data = new TierData[] {
-		new TierData(0.3 * 255, 1 * 255, 12, 0.02, 2402), //t1
-		new TierData( 0.3 * 255, 0.57 * 255, 22, 0.06, 1883), //t2
-		new TierData( 0.3 * 255, 0.49 * 255, 26, 0.12, 1398), //t3
-		new TierData( 0.3 * 255, 0.44 * 255, 31, 0.20, 924), //t4
-		new TierData( 0.23 * 255, 0.3 * 255, 48, 0.30, 1006), //t5
-		new TierData( 0.15 * 255, 0.2 * 255, 74, 0.42, 682), //t6
-		new TierData(0.09 * 255, 0.13 * 255, 123, 0.56, 679), //t7
-		new TierData(0.07 * 255, 0.1 * 255, 162, 0.72, 545), //t8
-		new TierData(0.06 * 255, 0.07 * 255, 244, 0.90, 459) //t9
+		new TierData(0.29 * 255, 0.46 * 255, 32, 0, 700), //t1
+		new TierData( 0.29 * 255, 0.46 * 255, 32, 0, 700), //t2
+		new TierData( 0.29 * 255, 0.46 * 255, 32, 0, 700), //t3
+		new TierData( 0.29 * 255, 0.46 * 255, 32, 0, 700), //t4
+		new TierData( 0.29 * 255, 0.46 * 255, 32, 0, 700), //t5
+		new TierData( 0.29 * 255, 0.46 * 255, 32, 0, 700), //t6
+		new TierData(0.29 * 255, 0.46 * 255, 32, 0, 700), //t7
+		new TierData(0.29 * 255, 0.46 * 255, 32, 0, 700), //t8
+		new TierData(0.29 * 255, 0.46 * 255, 32, 0, 700) //t9
 	};
 
 	public final double lowChance;
