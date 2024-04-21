df1d5a8c90e0fe12820b077fd187117fd1417efd
pwatts6060 <aaa>
2024-04-21 20:08:50 +0400

    Increase hiscores cache size

--- src/main/java/com/lmsnotifier/LMSHiscores.java
+++ src/main/java/com/lmsnotifier/LMSHiscores.java
@@ -5,6 +5,8 @@ import net.runelite.client.hiscore.*;
 import net.runelite.client.util.Text;
 
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 class LMSHiscores
@@ -12,10 +14,16 @@ class LMSHiscores
 	@Inject
 	private HiscoreManager hiscoreManager;
 
+	private final int CACHE_SIZE = 2000;
+	private static final Map<String, LMSRank> cache = new HashMap<>();
+
 	LMSRank getRankFor(String username)
 	{
 		final String name = Text.sanitize(username);
 		log.debug("Looking up hiscores for {}", username);
+		if (cache.containsKey(name)) {
+			return cache.get(name);
+		}
 		final HiscoreEndpoint endPoint = HiscoreEndpoint.NORMAL;
 		HiscoreResult result = hiscoreManager.lookupAsync(name, endPoint);
 		if (result != null) {
@@ -23,7 +31,12 @@ class LMSHiscores
 			int score = hiscoreSkill.getLevel();
 			int rank = hiscoreSkill.getRank();
 			log.debug("Retrieved hiscores for {} {} {}", username, rank, score);
-			return new LMSRank(rank, score);
+			LMSRank lmsRank = new LMSRank(rank, score);
+			cache.put(name, lmsRank);
+			if (cache.size() > CACHE_SIZE) {
+				cache.clear();
+			}
+			return lmsRank;
 		}
 		return null;
 	}
