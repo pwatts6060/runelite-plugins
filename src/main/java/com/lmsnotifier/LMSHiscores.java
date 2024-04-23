2986fec92b5b1510224d3e4992fe91cb728d2e6f
pwatts6060 <aaa>
2024-04-23 15:40:53 +0400

    Replace clear when full cache with guava cache

--- src/main/java/com/lmsnotifier/LMSHiscores.java
+++ src/main/java/com/lmsnotifier/LMSHiscores.java
@@ -1,12 +1,13 @@
 package com.lmsnotifier;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.client.hiscore.*;
 import net.runelite.client.util.Text;
 
 import javax.inject.Inject;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 class LMSHiscores
@@ -14,16 +15,16 @@ class LMSHiscores
 	@Inject
 	private HiscoreManager hiscoreManager;
 
-	private final int CACHE_SIZE = 2000;
-	private static final Map<String, LMSRank> cache = new HashMap<>();
+	private final Cache<String, LMSRank> cache = CacheBuilder.newBuilder().initialCapacity(30).maximumSize(2000).expireAfterAccess(60, TimeUnit.MINUTES).build();
 
 	LMSRank getRankFor(String username)
 	{
 		final String name = Text.sanitize(username);
-		log.debug("Looking up hiscores for {}", username);
-		if (cache.containsKey(name)) {
-			return cache.get(name);
+		LMSRank cachedRank = cache.getIfPresent(name);
+		if (cachedRank != null) {
+			return cachedRank;
 		}
+		log.debug("Looking up hiscores for {}", username);
 		final HiscoreEndpoint endPoint = HiscoreEndpoint.NORMAL;
 		HiscoreResult result = hiscoreManager.lookupAsync(name, endPoint);
 		if (result != null) {
@@ -33,9 +34,6 @@ class LMSHiscores
 			log.debug("Retrieved hiscores for {} {} {}", username, rank, score);
 			LMSRank lmsRank = new LMSRank(rank, score);
 			cache.put(name, lmsRank);
-			if (cache.size() > CACHE_SIZE) {
-				cache.clear();
-			}
 			return lmsRank;
 		}
 		return null;
